6defd3c60c83a6a48c24d7070836bbcc3d8df086
SAC-CS <huynh_durendal@sac.edu>
2015-09-25 10:34:33 -0700

    Test GitHub

---
+++ src/sac/edu/cs112/fall15/Calculator.java
@@ -0,0 +1,43 @@
+package sac.edu.cs112.fall15;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		Scanner op = new Scanner(System.in);
+		float num1;
+		System.out.print("Enter Number A : ");
+		num1 = input.nextFloat();
+		float num2;
+		System.out.print("Enter Number B : ");
+		num2 = input.nextFloat();
+		System.out.println("Enter type your operations: plus / minus / mult / div");
+		String choose = op.next();
+		//float danswer = num1 / num2;
+		if (choose.equals("plus")) {
+			System.out.println("Your answer is: " + num1 + "+" + num2 + "="
+					+ (num1 + num2));
+		} // I think my println is a mess , is there anyway to improve it ?
+		else if (choose.equals("minus")) {
+			System.out.println("Your answer is: " + num1 + "-" + num2 + "="
+					+ (num1 - num2));
+		} else if (choose.equals("mult")) {
+			System.out.println("Your answer is: " + num1 + "*" + num2 + "="
+					+ (num1 * num2));
+		} else if (choose.equals("div")) // I still wonder why (choose ==
+											// ("div"))not work
+		{
+			System.out.println("Your answer is: " + num1 + "/" + num2 + "="
+					+ (num1 / num2));
+		}
+		
+		input.close();
+		op.close();
+
+	
+	}
+
+}
